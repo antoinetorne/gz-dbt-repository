e0147e17a550b845b48c24ef551d1e13d7ace9ec
antoinelewagon@gmail.com <antoinelewagon@gmail.com>
2024-10-29 14:52:39 +0000

    added operational margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,9 +1,9 @@
-SELECT
-orders_id,
-date_date,
-ROUND(SUM(revenue),2) AS total_revenue, 
-ROUND(SUM(quantity),2) AS total_quantities,
-ROUND(SUM(purchase_cost),2) AS total_cost,
-ROUND(SUM(margin),2) AS total_margin,
-FROM {{ ref('int_sales_margin') }}
-GROUP BY orders_id, date_date
+select
+    orders_id,
+    date_date,
+    round(sum(revenue), 2) as total_revenue,
+    round(sum(quantity), 2) as total_quantities,
+    round(sum(purchase_cost), 2) as total_cost,
+    round(sum(margin), 2) as total_margin,
+from {{ ref("int_sales_margin") }}
+group by orders_id, date_date
