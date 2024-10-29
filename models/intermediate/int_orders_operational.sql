c1bb41336523e850e3e18f36a9f75fc75c0a86f8
antoinelewagon@gmail.com <antoinelewagon@gmail.com>
2024-10-29 15:34:00 +0000

    updated

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,8 +2,8 @@ select
     omargin.orders_id,
     omargin.date_date,
     round(sum(omargin.total_margin+oship.shipping_fee-oship.logcost-oship.ship_cost),2) as operational_margin,
-    round(sum(omargin.total_revenue),2) AS revenue
-    round(sum(omargin.total_cost),2) AS purchase_cost
+    round(sum(omargin.total_revenue),2) AS revenue,
+    round(sum(omargin.total_cost),2) AS purchase_cost,
     round(sum(omargin.total_quantities),2) AS qty,
     round(sum(omargin.total_margin),2) AS margin,
     round(sum(oship.shipping_fee),2) AS shipping_fee,
