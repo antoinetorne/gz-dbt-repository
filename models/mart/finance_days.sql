15ecc6b545ab310433ac9271077303eab76d0068
Antoine Torné <antoinelewagon@gmail.com>
2024-10-30 10:00:22 +0000

    materialized table finance days

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,17 +1,16 @@
-SELECT 
+ {{ config(materialized="table") }}
+
+
+select
     ordop.date_date,
     count(ordop.orders_id) as nb_transactions,
-    ROUND(sum(ordop.revenue),2) as daily_revenue,
-    ROUND(sum(ordop.revenue)/count(ordop.orders_id),2) as average_basket,
-    ROUND(sum(ordop.margin),2) as daily_margin, 
---operational_margin,
-    ROUND(sum(ordop.purchase_cost),2) as daily_purchase_cost,
-    ROUND(sum(ordop.shipping_fee),2) AS daily_shipping_fees, 
-    ROUND(sum(ordop.log_cost),2) AS daily_log_costs,
-    ROUND(sum(ordop.qty),2) as daily_qty,
-from {{ ref('int_orders_operational') }} as ordop
+    round(sum(ordop.revenue), 2) as daily_revenue,
+    round(sum(ordop.revenue) / count(ordop.orders_id), 2) as average_basket,
+    round(sum(ordop.margin), 2) as daily_margin,
+    -- operational_margin,
+    round(sum(ordop.purchase_cost), 2) as daily_purchase_cost,
+    round(sum(ordop.shipping_fee), 2) as daily_shipping_fees,
+    round(sum(ordop.log_cost), 2) as daily_log_costs,
+    round(sum(ordop.qty), 2) as daily_qty,
+from {{ ref("int_orders_operational") }} as ordop
 group by date_date
-    
-    
-    
-    
